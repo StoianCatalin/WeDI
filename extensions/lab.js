d3ea8c7bb438f6d3a96ff00c7c0097b165368ebc
goldlekx <aurelian.alexandrescuaa@gmail.com>
2018-01-27 16:14:13 +0200

    added export json and html

--- extensions/lab.js
+++ extensions/lab.js
@@ -1,6 +1,26 @@
 (function() {
     return function (param) {
         var data = [];
+        var numbers = param.text.match(/([0-9]{3,4}[-. ]?[0-9]{3,4}[-. ]?[0-9]{3,4})/gi);
+        if (numbers) {
+            data = numbers.map(function(number) {
+                return {
+                    number: number,
+                    url: param.url
+            }
+            });
+        }
+        var uniq = new Set(data.map(function(e) {return JSON.stringify(e); } ));
+
+        var res = Array.from(uniq).map(function(e) { return JSON.parse(e)});
+
+        return res;
+    };
+}());
+
+(function() {
+    return function (param) {
+        var data = [];
         var emails = param.text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
         if (emails) {
             data = emails.map(function(email) {
@@ -17,4 +37,3 @@
         return res;
     };
 }());
-
